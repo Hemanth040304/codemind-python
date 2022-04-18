d62c62c4312fdf7dff74d2a1332c3dd3484ec74d
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-04-18 23:01:10 +0530

    n = int(input()) sum = 0 temp = n while temp:     sum+=temp%10     temp//=10 if n%sum==0:     print(True) else:     print(False)

--- Harshed_number.py
+++ Harshed_number.py
@@ -1,10 +1,10 @@
 n = int(input())
-sum=0
-temp=n
-while n!=0:
-    sum+=n%10
-    n//=10
-if temp%sum==0:
+sum = 0
+temp = n
+while temp:
+    sum+=temp%10
+    temp//=10
+if n%sum==0:
     print(True)
 else:
     print(False)
