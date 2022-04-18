c40be9da262b4c893f2dbe6dac0d3cda9680ece0
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-04-18 23:06:51 +0530

    n = int(input()) first_number=0 second_number=1 print(first_number,end=' ') print(second_number,end=' ') while n!=2:     next_number=first_number+second_number     print(next_number,end=' ')     first_number=second_number     second_number=next_number     n-=1

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+n = int(input())
+first_number=0
+second_number=1
+print(first_number,end=' ')
+print(second_number,end=' ')
+while n!=2:
+    next_number=first_number+second_number
+    print(next_number,end=' ')
+    first_number=second_number
+    second_number=next_number
+    n-=1
