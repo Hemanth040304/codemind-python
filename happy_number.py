5ece0222670a637962364c1540521c1ae5bf5abf
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-05-17 17:58:16 +0530

    number = int(input()) sum = 0 while 1:     while number!=0:         sum+=(number%10)**2         number//=10     if sum==1 or sum==7:         print(True)         break     elif sum<10:         print(False)         break     else:         number=sum         sum=0

---
+++ happy_number.py
@@ -0,0 +1,15 @@
+number = int(input())
+sum = 0
+while 1:
+    while number!=0:
+        sum+=(number%10)**2
+        number//=10
+    if sum==1 or sum==7:
+        print(True)
+        break
+    elif sum<10:
+        print(False)
+        break
+    else:
+        number=sum
+        sum=0
