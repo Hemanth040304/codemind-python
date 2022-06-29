118ba511600f2d4bcdc7f3a952fad83611041932
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-06-29 15:33:56 +0530

    n = int(input()) arr = list(map(int,input().split())) count = 0 if arr[0]>arr[1]:     for i in range(n-1):         if i%2==0 and arr[i]>arr[i+1]:             count+=1         elif i%2 and arr[i]<arr[i+1]:             count+=1     if count==n-1:         print("yes")     else:         print("no") else:     for i in range(n-1):         if i%2==0 and arr[i]<arr[i+1]:             count+=1         elif i%2 and arr[i]>arr[i+1]:             count+=1     if count==n-1:         print("yes")     else:         print("no")

--- Wave_array.py
+++ Wave_array.py
@@ -1,23 +1,23 @@
-x = int(input())
-l = list(map(int,input().split()))
-c=0
-if l[0]<l[1]:
-    for i in range(1,x-1,2):
-        if (l[i-1]<l[i] and l[i]>l[i-1]):
-            c+=1
-        else:
-            print('no')
-            c=0
-            break
-    if c!=0:
+n = int(input())
+arr = list(map(int,input().split()))
+count = 0
+if arr[0]>arr[1]:
+    for i in range(n-1):
+        if i%2==0 and arr[i]>arr[i+1]:
+            count+=1
+        elif i%2 and arr[i]<arr[i+1]:
+            count+=1
+    if count==n-1:
         print("yes")
+    else:
+        print("no")
 else:
-    for i in range(1,x-1,2):
-        if (l[i-1]>l[i] and l[i]<l[i-1]):
-            c+=1
-        else:
-            print('no')
-            c=0
-            break
-    if c!=0:
+    for i in range(n-1):
+        if i%2==0 and arr[i]<arr[i+1]:
+            count+=1
+        elif i%2 and arr[i]>arr[i+1]:
+            count+=1
+    if count==n-1:
         print("yes")
+    else:
+        print("no")
