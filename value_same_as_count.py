6d3f5e7f2d5773b91a95de8f22e3d88ef9651476
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-06-30 15:54:27 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in sorted(set(a),key=a.index):     if a.count(i)==i:         c+=1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,9 +1,7 @@
-n = int(input())
-count=0
-arr = list(map(int,input().split()))
-for i in range(n):
-    c=arr.count(arr[i])
-    if c==arr[i]:
-        count+=1
-        arr[i]=0
-print(count)
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+for i in sorted(set(a),key=a.index):
+    if a.count(i)==i:
+        c+=1
+print(c)
