a130def49ccfbee466323a8b74f78a4ff3f13709
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-07-01 22:16:36 +0530

    n,m = map(int,input().split()) c = 0 for i in range(n):     a = list(map(int,input().split()))     b = a.copy()     b.sort()     q=b[::-1]     if a==b or a==q:         c+=1 print(c)

--- Count_sorted_rows_in_a_matrix.py
+++ Count_sorted_rows_in_a_matrix.py
@@ -1,17 +1,10 @@
 n,m = map(int,input().split())
 c = 0
-b = []
 for i in range(n):
     a = list(map(int,input().split()))
-    b.append(a)
-    co = 0
-    ca = 0
-    for j in range(m-1):
-        if a[j]<a[j+1]:
-            co+=1
-    for j in range(m-1):
-        if a[j]>a[j+1]:
-            ca+=1
-    if co==m-1 or ca==m-1:
+    b = a.copy()
+    b.sort()
+    q=b[::-1]
+    if a==b or a==q:
         c+=1
 print(c)
