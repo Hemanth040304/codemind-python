1d69f414bf9014a590982d1197f6f1c6c4d6cdb5
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-07-05 14:55:53 +0530

    s1 = input().lower() s2 = input().lower() a = '' for i in s1:     if i not in s2 and i not in a:         a+=i for i in s2:     if i not in s1 and i not in a:         a+=i a=sorted(a) a = str(a) a = a.replace("[","") a = a.replace("]","") a = a.replace("'","") a = a.replace(" ","") a = a.replace(",","") print(a)

--- non_common_characters.py
+++ non_common_characters.py
@@ -1,21 +1,17 @@
-a = input()
-b = input()
-a,b = a.replace(" ",""),b.replace(" ","")
-a,b = a.lower(),b.lower()
-a,b = set(a),set(b)
-a,b = list(a),list(b)
-c = []
-for i in range(0,len(a)):
-    if a[i] not in b:
-        c.append(a[i])
-for i in range(0,len(b)):
-    if b[i] not in a and b[i] not in c:
-        c.append(b[i])
-c.sort()
-c = str(c)
-c = c.replace(",","")
-c = c.replace("'","")
-c = c.replace("[","")
-c = c.replace("]","")
-c = c.replace(" ","")
-print(c)
+s1 = input().lower()
+s2 = input().lower()
+a = ''
+for i in s1:
+    if i not in s2 and i not in a:
+        a+=i
+for i in s2:
+    if i not in s1 and i not in a:
+        a+=i
+a=sorted(a)
+a = str(a)
+a = a.replace("[","")
+a = a.replace("]","")
+a = a.replace("'","")
+a = a.replace(" ","")
+a = a.replace(",","")
+print(a)
