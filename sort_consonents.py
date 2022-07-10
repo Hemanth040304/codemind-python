66ab48300e431e5fef68ad7c319dc11e1c0f8233
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-07-10 16:51:39 +0530

    s = input() s = s.split(" ") for i in range(len(s)):     a = list(s[i])     for j in range(len(a)):         for k in range(len(a)):             if a[j]<a[k] and a[j] not in 'aeiou' and a[k] not in 'aeiou':                 a[j],a[k]=a[k],a[j]     a = str(a)     a = a.replace("[","")     a = a.replace("]","")     a = a.replace(" ","")     a = a.replace(",","")     a = a.replace("[","")     a = a.replace("'","")     print(a,end=" ")

---
+++ sort_consonents.py
@@ -0,0 +1,16 @@
+s = input()
+s = s.split(" ")
+for i in range(len(s)):
+    a = list(s[i])
+    for j in range(len(a)):
+        for k in range(len(a)):
+            if a[j]<a[k] and a[j] not in 'aeiou' and a[k] not in 'aeiou':
+                a[j],a[k]=a[k],a[j]
+    a = str(a)
+    a = a.replace("[","")
+    a = a.replace("]","")
+    a = a.replace(" ","")
+    a = a.replace(",","")
+    a = a.replace("[","")
+    a = a.replace("'","")
+    print(a,end=" ")
