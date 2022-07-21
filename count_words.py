81dd1edf192861d089ecfe8720a42957388088cd
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-07-21 14:27:12 +0530

    s = input().split(" ") c = 0 vo = "aeiouAEIOU" for i in s:     if i[0] in vo and i[-1] not in vo:         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,9 +1,7 @@
-n = input()
-n = n.lower()
-n = n.split(" ")
-count = 0
-for i in range(len(n)):
-    a = " ".join(n[i])
-    if a[0] in 'aeiou' and a[len(a)-1] in 'qwrtypsdfghjklzxcvbnm':
-        count += 1
-print(count)
+s = input().split(" ")
+c = 0
+vo = "aeiouAEIOU"
+for i in s:
+    if i[0] in vo and i[-1] not in vo:
+        c+=1
+print(c)
