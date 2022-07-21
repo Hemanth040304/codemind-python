520351d5c126f0716fff0ba5858e86305968c87c
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-07-21 14:23:39 +0530

    s = input().lower() s = s.replace(" ","") s = set(s) s = list(s) s.sort() for i in s:     print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,17 +1,7 @@
-n = input()
-n = n.lower()
-n = n.replace(" ","")
-s = []
-for i in range(0,len(n)):
-    c = n.count(n[i])
-    if c==1:
-        s.append(n[i])
-s.sort()
-s = str(s)
-s = s.replace(",","")
-s = s.replace("[","")
-s = s.replace("]","")
-s = s.replace(",","")
-s = s.replace("'","")
+s = input().lower()
 s = s.replace(" ","")
-print(s)
+s = set(s)
+s = list(s)
+s.sort()
+for i in s:
+    print(i,end='')
