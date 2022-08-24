0a3d9c0c7905e55ef02a0e6c9609e9efb87dcd15
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-08-24 13:43:02 +0530

    s1 = input().lower() s2 = input().lower() s1 = set(s1) s2 = set(s2) s1 = str(s1) s2 = str(s2) s1=s1.replace(" ",'') s2=s2.replace(" ",'') s1=s1.replace("[",'') s2=s2.replace("[",'') s1=s1.replace("]",'') s2=s2.replace("]",'') s1=s1.replace(",",'') s2=s2.replace(",",'') s1=s1.replace("'",'') s2=s2.replace("'",'') s1=s1.replace(" ",'') s2=s2.replace(" ",'') c=0 for i in s1:     if i not in s2:         c+=1 for i in s2:     if i not in s1:         c+=1 print(c)

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,21 +1,26 @@
-a = input()
-b = input()
-a,b = a.replace(" ",""),b.replace(" ","")
-a,b = a.lower(),b.lower()
-a,b = set(a),set(b)
-a,b = list(a),list(b)
-c = []
-for i in range(0,len(a)):
-    if a[i] not in b:
-        c.append(a[i])
-for i in range(0,len(b)):
-    if b[i] not in a and b[i] not in c:
-        c.append(b[i])
-c.sort()
-c = str(c)
-c = c.replace(",","")
-c = c.replace("'","")
-c = c.replace("[","")
-c = c.replace("]","")
-c = c.replace(" ","")
-print(len(c))
+s1 = input().lower()
+s2 = input().lower()
+s1 = set(s1)
+s2 = set(s2)
+s1 = str(s1)
+s2 = str(s2)
+s1=s1.replace(" ",'')
+s2=s2.replace(" ",'')
+s1=s1.replace("[",'')
+s2=s2.replace("[",'')
+s1=s1.replace("]",'')
+s2=s2.replace("]",'')
+s1=s1.replace(",",'')
+s2=s2.replace(",",'')
+s1=s1.replace("'",'')
+s2=s2.replace("'",'')
+s1=s1.replace(" ",'')
+s2=s2.replace(" ",'')
+c=0
+for i in s1:
+    if i not in s2:
+        c+=1
+for i in s2:
+    if i not in s1:
+        c+=1
+print(c)
