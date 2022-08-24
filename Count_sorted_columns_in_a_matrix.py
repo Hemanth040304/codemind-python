c2f8f83046b10a32b0092ac1b2b61cd0a04248a8
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-08-24 11:13:18 +0530

    n,m=map(int,input().split()) c=0 p=[] for l in range(m):     p.append([]) for i in range(n):     a=list(map(int,input().split()))     for j in range(m):         p[j].append(a[j]) for k in p:     b=k.copy()     b.sort()     q=b[::-1]     if k==b or k==q:         c+=1 print(c)

--- Count_sorted_columns_in_a_matrix.py
+++ Count_sorted_columns_in_a_matrix.py
@@ -1,16 +1,16 @@
-n,m = map(int,input().split())
-c = 0
-b = []
-for i in range(m):
-    b.append([])
+n,m=map(int,input().split())
+c=0
+p=[]
+for l in range(m):
+    p.append([])
 for i in range(n):
-    a = list(map(int,input().split()))
+    a=list(map(int,input().split()))
     for j in range(m):
-        b[j].append(a[j])
-for i in b:
-    p=i.copy()
-    p.sort()
-    q = p[::-1]
-    if i==p or i==q:
+        p[j].append(a[j])
+for k in p:
+    b=k.copy()
+    b.sort()
+    q=b[::-1]
+    if k==b or k==q:
         c+=1
 print(c)
