5f01eb331e0c8231d6d5f3a4e279cd5346f7d641
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-08-24 14:06:46 +0530

    s = input() s = s.split(" ") for i in range(len(s)):     a = list(s[i])     for j in range(len(a)):         for k in range(len(a)):             if a[j]<a[k] and a[j] not in '`~!<>?,./@#$%^&*()_+-={}|[]\:;"' and a[k] not in '`~!<>?,./@#$%^&*()_+-={}|[]\:;"':                 a[j],a[k]=a[k],a[j]     a = str(a)     a = a.replace("[","")     a = a.replace("]","")     a = a.replace(" ","")     a = a.replace(",","")     a = a.replace("[","")     a = a.replace("'","")     print(a,end=" ")

--- sort_words_of_a_string.py
+++ sort_words_of_a_string.py
@@ -4,7 +4,7 @@ for i in range(len(s)):
     a = list(s[i])
     for j in range(len(a)):
         for k in range(len(a)):
-            if a[j]<a[k] and a[j] not in '!@#$%^&*()_+-={}|[]\:";<>,.?/~' and a[k] not in '!@#$%^&*()_+-={}|[]\:";<>,.?/~':
+            if a[j]<a[k] and a[j] not in '`~!<>?,./@#$%^&*()_+-={}|[]\:;"' and a[k] not in '`~!<>?,./@#$%^&*()_+-={}|[]\:;"':
                 a[j],a[k]=a[k],a[j]
     a = str(a)
     a = a.replace("[","")
