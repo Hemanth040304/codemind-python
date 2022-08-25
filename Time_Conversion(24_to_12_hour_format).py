2b625b574b7e76016f7ccd048435718e7cce8454
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-08-25 13:54:57 +0530

    s = input() if s[0]=='0' and s[1]=='0':     print("12"+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='1':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='2':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='3':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='4':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='5':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='6':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='7':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='8':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='0' and s[1]=='9':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='1' and s[1]=='0':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='1' and s[1]=='1':     print(s[0]+s[1]+s[2]+s[3]+s[4],"AM") elif s[0]=='1' and s[1]=='2':     print(s[0]+s[1]+s[2]+s[3]+s[4],"PM") elif s[0]=='1' and s[1]=='3':     print('01'+s[2]+s[3]+s[4],"PM") elif s[0]=='1' and s[1]=='4':     print('02'+s[2]+s[3]+s[4],"PM") elif s[0]=='1' and s[1]=='5':     print('03'+s[2]+s[3]+s[4],"PM") elif s[0]=='1' and s[1]=='6':     print('04'+s[2]+s[3]+s[4],"PM") elif s[0]=='1' and s[1]=='7':     print('05'+s[2]+s[3]+s[4],"PM") elif s[0]=='1' and s[1]=='8':     print('06'+s[2]+s[3]+s[4],"PM") elif s[0]=='1' and s[1]=='9':     print('07'+s[2]+s[3]+s[4],"PM") elif s[0]=='2' and s[1]=='0':     print('08'+s[2]+s[3]+s[4],"PM") elif s[0]=='2' and s[1]=='1':     print('09'+s[2]+s[3]+s[4],"PM") elif s[0]=='2' and s[1]=='2':     print('10'+s[2]+s[3]+s[4],"PM") elif s[0]=='2' and s[1]=='3':     print('11'+s[2]+s[3]+s[4],"PM") elif s[0]=='2' and s[1]=='4':     print('12'+s[2]+s[3]+s[4],"PM")

---
+++ Time_Conversion(24_to_12_hour_format).py
@@ -0,0 +1,51 @@
+s = input()
+if s[0]=='0' and s[1]=='0':
+    print("12"+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='1':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='2':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='3':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='4':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='5':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='6':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='7':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='8':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='0' and s[1]=='9':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='1' and s[1]=='0':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='1' and s[1]=='1':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"AM")
+elif s[0]=='1' and s[1]=='2':
+    print(s[0]+s[1]+s[2]+s[3]+s[4],"PM")
+elif s[0]=='1' and s[1]=='3':
+    print('01'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='1' and s[1]=='4':
+    print('02'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='1' and s[1]=='5':
+    print('03'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='1' and s[1]=='6':
+    print('04'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='1' and s[1]=='7':
+    print('05'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='1' and s[1]=='8':
+    print('06'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='1' and s[1]=='9':
+    print('07'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='2' and s[1]=='0':
+    print('08'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='2' and s[1]=='1':
+    print('09'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='2' and s[1]=='2':
+    print('10'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='2' and s[1]=='3':
+    print('11'+s[2]+s[3]+s[4],"PM")
+elif s[0]=='2' and s[1]=='4':
+    print('12'+s[2]+s[3]+s[4],"PM")
