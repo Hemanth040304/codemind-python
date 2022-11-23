3a39ef1b2ce48ecd54bf045b0158a8a8889c8e8e
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-11-23 06:55:25 +0530

    Create Password Generator.py

---
+++ Password Generator.py
@@ -0,0 +1,16 @@
+s=input()
+a1=list(s.split(","))
+for i in a1:
+    a2=list(i.split(":"))
+    n1=a2[0]
+    n2=a2[1]
+    m='0'
+    for i in n2:
+        if ord(i)>ord(m) and ord(i)-48<=len(n1):
+            m=i
+    nd=ord(m)-48
+    n=n1*10
+    if m=='0':
+        print("X",end="")
+    else:
+        print(n[nd-1],end="")
