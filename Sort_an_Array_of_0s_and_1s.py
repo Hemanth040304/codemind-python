93db12157161fc2f59890cda80f9fe303af55fcb
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-11-26 20:05:21 +0530

    n = int(input()) arr = list(map(int,input().split())) arr.sort() print(*arr)

--- Sort_an_Array_of_0s_and_1s.py
+++ Sort_an_Array_of_0s_and_1s.py
@@ -1,8 +1,4 @@
 n = int(input())
 arr = list(map(int,input().split()))
-for i in range(0,n):
-    if arr[i]==0:
-        print(arr[i],end=" ")
-for i in range(0,n):
-    if arr[i]==1:
-        print(arr[i],end=" ")
+arr.sort()
+print(*arr)
