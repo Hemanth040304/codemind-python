f23701edc6dcc1d6dc6a1701ab1c2382d3b109f9
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-11-27 10:26:45 +0530

    for _ in range(int(input())):     a,b = map(int,input().split())     c = 0     for i in range(0,b):         if i*i%b==a:             c = 1             print(i)             break     if c==0:         print(-1)

---
+++ Monk_and_Square_Root.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    a,b = map(int,input().split())
+    c = 0
+    for i in range(0,b):
+        if i*i%b==a:
+            c = 1
+            print(i)
+            break
+    if c==0:
+        print(-1)
